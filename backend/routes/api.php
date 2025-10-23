20bd0c8e9ef98804e542c2cffe26acfa6dea6809
Saidosik <Sadiliofimperia@gmail.com>
2025-10-23 20:51:14 +0300

    said 0.0.1

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/catalog', function (Request $request) {
+    return response()->json([
+        'data' => [
+            ['id' => 1, 'name' => 'Товар 1'],
+            ['id' => 2, 'name' => 'Товар 2'],
+        ]
+    ]);
+});
