b220ad92bcd1099218f3812ab4b9504949d8ae7b
kyusak0 <chirkinvova4@gmail.com>
2025-12-10 14:56:38 +0300

    just commit
    no changes

--- config/cors.php
+++ config/cors.php
@@ -10,6 +10,7 @@ return [
         'http://api.localhost.test:8001',
         'http://app.localhost.test:3000',
         'http://api.localhost.test',
+        'http://localhost:3000'
     ],
 
     'allowed_origins_patterns' => [],
